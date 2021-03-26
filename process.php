178ab5c5dba636d6fa110a011cc636de9b29e716
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-03-26 07:37:54 +0200

    session set

--- process.php
+++ process.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $mysqli= new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli)); //connect to DB
 
 if(isset($_POST['save'])) {
@@ -8,4 +10,21 @@ if(isset($_POST['save'])) {
     // Performs a query(uzklausa) on the database
     $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or
     die($mysqli->error);
+
+    $_SESSION['message'] = "Record has been saved";
+    $_SESSION['msg_type'] = "success";
+
+    header("location: index.php");
+    die;
+}
+
+if(isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM data WHERE $id=$id") or die($mysqli);
+
+    $_SESSION['message'] = "Record has been deleted";
+    $_SESSION['msg_type'] = "danger";
+
+    header("location: index.php");
+    die;
 }
